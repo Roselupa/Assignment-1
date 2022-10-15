d4e18bf400e9ed26dffea24236c05f271b0f0850
Roselupa <113816220+Roselupa@users.noreply.github.com>
2022-10-15 12:03:22 +0530

    Add files via upload

---
+++ rose_ibm.ino
@@ -0,0 +1,27 @@
+ import random
+
+i = 2
+
+while(True):
+
+a=random.randint(10,100)
+
+b=random.randint(10,100)
+
+if(a>35 and b < 65 ):
+
+print("HIGH TEMPERATURE AND HUMIDITY OF:",a,b,"%","ALARM IS ON")
+
+elif ( a < 35 and b > 65 :
+
+print("NORMAL TEMPERATURE AND HUMIDITY OF:",a,b,"%", "ALARM IS OFF")
+
+if(i < 20)
+
+i=i+1
+
+random
+
+else:
+
+break
